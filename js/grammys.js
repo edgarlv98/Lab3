05aca3035d32aa18c5366a1b028446b360ab88fd
Edgar <edgarlv_1998@hotmail.com>
2019-02-15 17:06:45 -0600

    Ya funciona hasta descrpcion

--- js/grammys.js
+++ js/grammys.js
@@ -7,19 +7,32 @@ $.ajax({
 
         for( let i=0; i<data.fields.length; i++){
             new_html += `
-                <option value="${data.fields[i].field_id}">${data.fields[i].field}</option>
+                <option value="
+                    ${data.fields[i].field_id}">${data.fields[i].field}
+                </option>
             `;
         }
 
+        var id=0;
+
         $("#category_types").append(new_html);
         $("#category_types").on('change', function(event){
-            $("#category").text($(this).children("option:selected").text());
+            $("#field").text($(this).children("option:selected").text());
+            id=parseInt($("#category_types").val());
+            $("#description").text(data.fields[id-1].description);
+            console.log(id);
         });
 
-        for(let i=0; i<data.fields.field.length; i++){
-            
+        var id2=0;
+
+        for(let i=0; i<data.fields[id].categories.length; i++){
+            console.log(data.fields[id].categories[i].category_name);
+            console.log(data.fields[id].categories[i].description);
         }
 
+        for(let j=0; j<data.fields[id].categories[id2].nominees.length; j++){
+            console.log(data.fields[id].categories[id2].nominees[j].nominee);
+        }
     },
     error: function(error_msg){
         console.log(error_msg);
