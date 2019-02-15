c6ef748f7601ec1278d5991c0af84025f863f744
Edgar <edgarlv_1998@hotmail.com>
2019-02-15 10:59:29 -0600

    first commit

---
+++ js/grammys.js
@@ -0,0 +1,25 @@
+$.ajax({
+    url : "data/grammys.json",
+    type : "GET",
+    dataType : "json",
+    success: function(data){
+        let new_html = "";
+
+        for( let i=0; i<data.fields.length; i++){
+            new_html += `
+                <option value="${data.fields[i].field_id}">${data.fields[i].field}</option>
+            `;
+        }
+
+        $("#category_types").append(new_html);
+        $("#category_types").on('change', function(event){
+            $("#category").text($(this).children("option:selected").text());
+        });
+
+        for(let i=0; i<data.)
+
+    },
+    error: function(error_msg){
+        console.log(error_msg);
+    }
+});
