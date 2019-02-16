b9a215b75d78a1820470bce71f09aa99c1d96832
Edgar <edgarlv_1998@hotmail.com>
2019-02-15 18:18:33 -0600

    Despliega una sola categoria en pantalla

--- js/grammys.js
+++ js/grammys.js
@@ -25,6 +25,18 @@ $.ajax({
 
         var id2=0;
 
+        $("#category_types").on('change', function(event){
+            let id3=parseInt($("#category_types").val());
+            for(let i=0; i<data.fields[id3].categories.length; i++){
+                $("#category").text(data.fields[id3-1].categories[i].category_name);
+                $("#descCategory").text(data.fields[id3-1].categories[i].description);
+                for(let j=0; j<data.fields[id3].categories[j].nominees.length; j++){
+                    $("#descCategory").text(data.fields[id3-1].categories[i].nominees[j].nominee);
+                }
+            }
+        });
+
+/*
         for(let i=0; i<data.fields[id].categories.length; i++){
             console.log(data.fields[id].categories[i].category_name);
             console.log(data.fields[id].categories[i].description);
@@ -32,7 +44,10 @@ $.ajax({
 
         for(let j=0; j<data.fields[id].categories[id2].nominees.length; j++){
             console.log(data.fields[id].categories[id2].nominees[j].nominee);
+            console.log(data.fields[id].categories[id2].nominees[j].artist);
+            console.log(data.fields[id].categories[id2].nominees[j].info);
         }
+*/
     },
     error: function(error_msg){
         console.log(error_msg);
